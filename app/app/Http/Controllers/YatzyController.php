61576ae53f36c59017791770e1ab80b83bb89f91
Daniel Andersson <d.andersson86@icloud.com>
2021-07-28 23:24:27 +0200

    Creating YatzyController methods for Yatzy.

--- app/app/Http/Controllers/YatzyController.php
+++ app/app/Http/Controllers/YatzyController.php
@@ -5,8 +5,8 @@
  */
 namespace App\Http\Controllers;
 
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-use Illuminate\Routing\Redirector;
 use Illuminate\View\View;
 use Daap19\Yatzy\Yatzy;
 
@@ -18,7 +18,7 @@ use Daap19\Yatzy\Yatzy;
 class YatzyController extends Controller
 {
     /**
-     * @description Render view with form for user input. This view is intended to start a game of Yatzy.
+     * @description Controller method to render view for initialization of a game of Yatzy.
      * @return View
      */
     final public function viewInit(): View
@@ -38,32 +38,29 @@ class YatzyController extends Controller
     }
 
 
+
     /**
      * @description Create new instance of class Yatzy and process response by saving to named session. Lastly redirect to route.
      * @param Request $request
-     * @return Redirector
+     * @return RedirectResponse
      */
-    final public function processInit(Request $request): Redirector
+    final public function processInit(Request $request): RedirectResponse
     {
         $yatzy = new Yatzy();
 
         $request->session()->put('yatzy', $yatzy);
 
-        $theSession = $request->session()->get('yatzy');
-
-        return redirect('/yatzy/view')->with($theSession);
+        return redirect('/yatzy/view');
     }
 
 
+
     /**
-     * Main view the result.
+     * @description Controller method to render main yatzy game view.
+     * @return View
      */
     final public function viewMain(): View
     {
-        $yatzy = new Yatzy();
-
-        session()->put('yatzy', $yatzy);
-
         $yatzy = session()->get('yatzy');
         $player = $yatzy->getCurrentPlayer();
         $diceHand = $player->getDiceHand();
@@ -75,6 +72,7 @@ class YatzyController extends Controller
             "selectScoresURL" => url("/yatzy__selectScores/view"),
             "round" => $yatzy->getRound(),
             "playerRolls" => $player->getRolls(),
+            "playerNumber" => $yatzy->getPlayerIndex() +1,
         ];
 
         if ($diceHand !== null) {
@@ -85,44 +83,43 @@ class YatzyController extends Controller
     }
 
 
+
     /**
-     * @method processMain()
-     * @description method to process POST action to return response object.
+     * @description Controller method to render main view.
+     * @param Request $request
+     * @return RedirectResponse
      */
-    final public function processMain(Request $request): Redirector
+    final public function processMain(Request $request): RedirectResponse
     {
         /* Catch POST request from dice__init form and store values to SESSION variable */
+        $input = $request->all();
         $yatzy = session()->get('yatzy');
+
         $player = $yatzy->getCurrentPlayer();
-        $submit = strval($_POST["submit"]);
+        $submit = strval($input["submit"]);
         $diceHand = $player->getDiceHand();
         $dices = $diceHand->getDices();
         $keepThese = [];
-        $input = $request->all();
-
-
-        ddd($input);
-
 
         /* Keep user selected dice values */
         if ($diceHand !== null) {
-            if (isset($_POST["dice-0"])) {
+            if (isset($input["dice-0"])) {
                 $keepThese[0] = $dices[0];
             }
 
-            if (isset($_POST["dice-1"])) {
+            if (isset($input["dice-1"])) {
                 $keepThese[1] = $dices[1];
             }
 
-            if (isset($_POST["dice-2"])) {
+            if (isset($input["dice-2"])) {
                 $keepThese[2] = $dices[2];
             }
 
-            if (isset($_POST["dice-3"])) {
+            if (isset($input["dice-3"])) {
                 $keepThese[3] = $dices[3];
             }
 
-            if (isset($_POST["dice-4"])) {
+            if (isset($input["dice-4"])) {
                 $keepThese[4] = $dices[4];
             }
 
@@ -135,9 +132,112 @@ class YatzyController extends Controller
 
         // Return the redirect through parent class ControllerBase destination depends on number of rolls.
         if ($player->getRolls() < 3) {
-            return $this->redirect(url("/yatzy/view"));
+            return redirect(url("/yatzy/view"));
         }
 
-        return $this->redirect(url("/yatzy__selectScores/view"));
+        return redirect(url("/yatzy/select/view"));
+    }
+
+
+
+    /**
+     * @description Renders view where player can select where to place her/his score.
+     * @return View
+     */
+    final public function viewSelect(): View
+    {
+        $yatzy = session()->get('yatzy');
+        $player = $yatzy->getCurrentPlayer();
+
+        $data = [
+            "header" => "Yatzy - Player Points Selection",
+            "message" => "Select where on the chart to place your points. Once your points for this round have been placed you can not place more points to this position. Try to make strategic placement for a higher sum total as your end result.",
+            "action" => url("/yatzy/select/process"),
+            "round" => $yatzy->getRound(),
+            "playerNumber" => $yatzy->getPlayerIndex() +1,
+            "graphicDices" => $yatzy->showGraphicDices($player->getDiceHand()),
+            "scoreBoard" => $yatzy->printYatzyScoreBoard(),
+            "scoreSelection" => $yatzy->scoreSelection(),
+            "scoreSum" => $player->getPlayerScoreSum(),
+        ];
+
+        return view("/yatzy_select", $data);
+    }
+
+
+
+    /**
+     * @description Controller method to recive and process request from user input.
+     * @param Request $request
+     * @return RedirectResponse
+     */
+    final public function processSelect(Request $request): RedirectResponse
+    {
+        $input = $request->all();
+        $yatzy = session()->get('yatzy');
+        $player = $yatzy->getCurrentPlayer();
+        $diceHand = $player->getDiceHand();
+        $lastRoll = $player->getLastRoll();
+        $maxScores = count($player->getPlayerScore());
+        $playerSavedScores = $player->getAmountOfScoresSaved();
+
+        /* Control where points are stored */
+        switch ($input["scoreSelect"]) {
+            case "0":
+                $player->saveScores($lastRoll, 1);
+                break;
+            case "1":
+                $player->saveScores($lastRoll, 2);
+                break;
+            case "2":
+                $player->saveScores($lastRoll, 3);
+                break;
+            case "3":
+                $player->saveScores($lastRoll, 4);
+                break;
+            case "4":
+                $player->saveScores($lastRoll, 5);
+                break;
+            case "5":
+                $player->saveScores($lastRoll, 6);
+                break;
+        }
+
+        $playerSavedScores++;
+        $diceHand->setForNextRound();
+        $player->setForNextRound();
+        $yatzy->setNextRound();
+
+        if ($playerSavedScores < $maxScores) {
+            // Return the redirect through parent class ControllerBase
+            return redirect(url("/yatzy/view"));
+        }
+
+        // Return the redirect through parent class ControllerBase if player saved $maxScores or more.
+        return redirect(url("/yatzy/result/view"));
+    }
+
+
+    /**
+     * @description
+     * @return View
+     */
+    final public function viewResult(): View
+    {
+        $yatzy = session()->get("yatzy");
+        $player = $yatzy->getCurrentPlayer();
+
+        $data = [
+            "header" => "Yatzy - Players Final Results",
+            "message" => "You have compleated this part of the game and here are your current scores.",
+            "action" => url("/yatzy__selectScores/process"),
+            "round" => $yatzy->getRound(),
+            "playerNumber" => $yatzy->getPlayerIndex() +1,
+            "scoreBoard" => $yatzy->printYatzyScoreBoard(),
+            "playerScores" => $player->getPlayerScore(),
+            "scoreSum" => $player->getPlayerScoreSum(),
+        ];
+
+        return view("yatzy_result", $data);
     }
 }
