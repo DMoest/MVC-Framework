c203f414a414bc06a06e1d6be5f7baf44d02a5ac
Daniel Andersson <d.andersson86@icloud.com>
2021-08-03 07:54:27 +0200

    Kmom05 - Start

--- app/resources/views/components/navigation-link.blade.php
+++ app/resources/views/components/navigation-link.blade.php
@@ -5,6 +5,6 @@
   $classes = Request::routeIs($route) ? 'text-white bg-blue-200 ' : 'hover:underline hover:text-blue-400';
 @endphp
 
-<a href='{{ route($route) }}' {{ $attributes->merge(['class' => 'px-6 rounded-sm ' . $classes]) }}>
+<a href='{{ route($route) }}' {{ $attributes->merge(['class' => 'px-6 rounded-sm font-link ' . $classes]) }}>
     {{ $slot }}
 </a>
