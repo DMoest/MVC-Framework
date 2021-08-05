34d61a57370aa54f32d8b2cbe4ceebf4eb43bb43
Daniel Andersson <d.andersson86@icloud.com>
2021-08-05 14:53:27 +0200

    Fixed broken link between authors page and individual book links.

--- app/resources/views/author.blade.php
+++ app/resources/views/author.blade.php
@@ -27,13 +27,13 @@
                     <x-table-row class="hover:cursor-pointer">
                         <th class="px-2">Book: </th>
 
-                        <x-td-link route="book/{{ $book->id }}"><p class="px-2 m-0 text-center"> {{ $key }} </p></x-td-link>
-                        <x-td-link route="book/{{ $book->id }}"><p class="px-2 m-0 text-right"> {{ $book->isbn }} </p></x-td-link>
-                        <x-td-link route="book/{{ $book->id }}"><p class="px-2 m-0 text-left"> {{ $book->title }} </p></x-td-link>
-                        <x-td-link route="book/{{ $book->id }}"><p class="px-2 m-0 text-center"> {{ $book->publisher }} </p></x-td-link>
+                        <x-td-link route="./../book/{{ $book->id }}"><p class="px-2 m-0 text-center"> {{ $key }} </p></x-td-link>
+                        <x-td-link route="./../book/{{ $book->id }}"><p class="px-2 m-0 text-right"> {{ $book->isbn }} </p></x-td-link>
+                        <x-td-link route="./../book/{{ $book->id }}"><p class="px-2 m-0 text-left"> {{ $book->title }} </p></x-td-link>
+                        <x-td-link route="./../book/{{ $book->id }}"><p class="px-2 m-0 text-center"> {{ $book->publisher }} </p></x-td-link>
                         <td><p class="px-2 m-0 text-right"> {{ $book->released }} </p></td>
                         <td><p class="px-2 m-0 text-right"> {{ $book->added_to_library }} </p></td>
-                        <x-td-link route="book/{{ $book->id }}">
+                        <x-td-link route="./../book/{{ $book->id }}">
                             <img src="{{ asset($book->picture) }}" class="w-14 p-2 m-0 text-right"/>
                         </x-td-link>
                     </x-table-row>
