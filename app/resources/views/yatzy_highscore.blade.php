d06b76a9182c82500ce40b775fdc8be9677f1755
Daniel Andersson <d.andersson86@icloud.com>
2021-08-08 22:22:02 +0200

    Polished the views for results and highscore a little.

--- app/resources/views/yatzy_highscore.blade.php
+++ app/resources/views/yatzy_highscore.blade.php
@@ -2,21 +2,18 @@
     <x-section>
 
         <h2 class="text-2xl text-center font-weight-bold font-header py-6">{{ $header }}</h2>
-        <p class="font-body">{{ $message }}</p>
+        <p class="text-center font-body">{{ $message }}</p>
 
         <x-table class="m-auto my-8 border-solid border-2 border-blue-200">
             {{-- Table explanatory row --}}
             <tr class="p-2 bg-blue-200">
-                <th class="p-1 text-center text-grey-500">Position</th>
                 <th class="p-1 text-center text-grey-500">Score</th>
                 <th class="p-1 text-center text-grey-500">Player</th>
                 <th class="p-1 text-center text-grey-500">Time</th>
             </tr>
 
-
             @foreach ($highscores as $score)
                 <x-table-row class="hover:cursor-pointer">
-                    <td><p class="px-2 m-0 text-center"> {{ $score->id }} </p></td>
                     <td><p class="px-2 m-0 text-center"> {{ $score->score }} </p></td>
                     <td><p class="px-2 m-0 text-center"> {{ $score->name }} </p></td>
                     <td><p class="px-2 m-0 text-center"> {{ $score->created_at }} </p></td>
--- app/resources/views/yatzy_result.blade.php
+++ app/resources/views/yatzy_result.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
     <x-section>
-        <x-form method="get"
+        <x-form method="post"
                 action="{{ $action }}"
                 class="p-4 bg-gray-100 border-2 border-solid border-gray-400 rounded-sm mb-6">
 
@@ -8,16 +8,27 @@
             <p class="pb-2 italic text-sm">{{ $message }}</p>
 
             @if (isset($playerScores))
-                <h3>Player {{ $playerNumber }}</h3>
+                <h3 class="font-weight-bold text-xl">Player {{ $playerNumber }}</h3>
 
                 <div>
                     @foreach($playerScores as $key => $value)
                         <div class="flex flex-row justify-between p-0 m-0">
-                            <p class=""><i class="dice-utf8 dice-{{ $key +1 }}"></i> Points: <b></b>{{ $value }}</b></p>
+                            <p><i class="dice-utf8 dice-{{ $key +1 }}"></i> Points: <b></b>{{ $value }}</b></p>
                         </div>
                     @endforeach
 
                     <p class="py-4">Player total score: {{ $scoreSum }}</p>
+                </div>
+            @endif
+
+            @if ($round >= 6)
+                <div class="p-4 flex flex-row gap-14">
+                    <label class="my-auto">
+                        <input type="text" name="name" required class="h-14 bg-blue-100 form-input inline-block italic" placeholder="Write your name here">
+                    </label>
+
+                    <button class="w-52 flex flex-row justify-around p-4 rounded-sm bg-green-400 uppercase font-link font-semibold hover:bg-green-300 hover:text-white" type="submit" name="submit" value="roll">
+                        Save Scores <x-icon-play class="w-4" /></button>
                 </div>
             @endif
 
