e555f307f1b1e606a2aa0ea42cc905e5f8014fa2
Daniel Andersson <d.andersson86@icloud.com>
2021-08-15 16:06:13 +0200

    Adding methods to handle routes for the pages/views: book, author, category and publisher.

--- app/app/Http/Controllers/LibraryController.php
+++ app/app/Http/Controllers/LibraryController.php
@@ -3,16 +3,21 @@
 namespace App\Http\Controllers;
 
 use App\Models\Book;
-use Illuminate\Http\Request;
-use Illuminate\View\View;
+use App\Models\Category;
+use App\Models\Author;
+use App\Models\Publisher;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+
 
 class LibraryController extends Controller
 {
     /**
      * @description Method to render view for full library.
-     * @return View
+     * @return Application|Factory|View
      */
-    public function viewLibrary():View
+    final public function viewLibrary(): Application|Factory|View
     {
         $data = [
             'header' => 'Daniels Library',
@@ -24,19 +29,58 @@ class LibraryController extends Controller
     }
 
 
+    /**
+     * @description Method to render view for a single book.
+     * @param Book $book
+     * @return Application|Factory|View
+     */
+    final public function viewBook(Book $book): Application|Factory|View
+    {
+        return view('book', [
+            'book' => $book,
+        ]);
+    }
 
-//    /**
-//     * @description Method to render view for a single book.
-//     * @return View
-//     */
-//    public function viewBook():View
-//    {
-//
-//        $data = [
-////            'header' => $book->titel,
-//            'message' => 'My book blabla bla bla blaaaaa....',
-//        ];
-//
-//        return view('book', $data);
-//    }
+
+    /**
+     * @description Method to render view for a single book.
+     * @param Category $category
+     * @return Application|Factory|View
+     */
+    final public function viewCategory(Category $category): Application|Factory|View
+    {
+        return view('category', [
+            'category' => $category,
+            'books' => $category->books->load('author', 'publisher'),
+        ]);
+    }
+
+
+    /**
+     * @description Methor to render view for an Author of books.
+     * @param Author $author
+     * @return Application|Factory|View
+     */
+    final public function viewAuthor(Author $author): Application|Factory|View
+    {
+        return view('author', [
+            'author' => $author,
+            'books' => $author->books->load('category', 'publisher'),
+        ]);
+    }
+
+
+    /**
+     * @description Method to render view for a Publisher.
+     * @param Publisher $publisher
+     * @return Application|Factory|View
+     */
+    final public function viewPublisher(Publisher $publisher): Application|Factory|View
+    {
+        return view('publisher', [
+            'publisher' => $publisher,
+            'authors' => $publisher->authors->load('books'),
+            'books' => $publisher->books->load('author', 'category'),
+        ]);
+    }
 }
